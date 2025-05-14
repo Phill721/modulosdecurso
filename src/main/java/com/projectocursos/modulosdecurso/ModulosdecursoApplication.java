26a7a1b985c06a9111b9889f5fcf55fc51ca8c04
Phillipe <ge.bonhomme@duocuc.clean>
2025-05-14 14:14:45 -0400

    CREADO REPOSITORIO

---
+++ src/main/java/com/projectocursos/modulosdecurso/ModulosdecursoApplication.java
@@ -0,0 +1,13 @@
+package com.projectocursos.modulosdecurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulosdecursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulosdecursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectocursos/modulosdecurso/ModulosdecursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectocursos.modulosdecurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulosdecursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
